20fbe7e85a817390ac4c202aced67baeec5c3b21
rohan <rohan@DESKTOP-2EN283H>
2020-10-05 20:29:44 -0700

    First Java code

---
+++ CSE360/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,26 @@
+package cse360assignment02;
+//rohan kapur assignment1 
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {
+    return 0;
+  }
+  
+  public void add (int value) {
+  }
+
+  public void subtract (int value) {
+  }
+
+  public String toString () {
+    return "";
+  }
+
+  public void clear() {
+  }
+}
